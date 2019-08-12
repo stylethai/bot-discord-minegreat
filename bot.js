ddcbc193635da9ce11bca518e5afd8ff705163e3
FaRosz <ball_k.p@hotmail.com>
2019-08-12 16:15:46 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const { prefix, token } = require('bot-discord-minegreat/blob/master/config.json');
+const { prefix, token } = require('./config.json');
 
 
 const bot = new Discord.Client();
